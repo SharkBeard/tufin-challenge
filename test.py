1339114aec8891c16b3c472389444ff2e2858e0b
Aaron Campbell <sharkbeardio@gmail.com>
2020-01-13 10:38:55 -0500

    updated tests

--- test.py
+++ test.py
@@ -6,10 +6,8 @@ from converter import convert_line
 
 class TestCombine(unittest.TestCase):
   def test_any(self):
-    line = 'any,0'
-    self.assertEqual(convert_line(line), 'any;0')
-    line = '1.0.0.255/any'
-    self.assertEqual(convert_line(line), line)
+    line = 'any,0,1.0.0.255/any'
+    self.assertEqual(convert_line(line), 'any;0;1.0.0.255/any')
   def test_sample(self):
     ips = ['10.10.10.0/24','10.10.11.0/24','10.10.12.0/24']
     self.assertEqual(combine_ips(ips), '10.10.10.0/23;10.10.12.0/24')
@@ -17,10 +15,8 @@ class TestCombine(unittest.TestCase):
     line = '1.2.3.4/255.255.0.0'
     self.assertEqual(convert_line(line), '1.2.3.4/255.255.0.0')
   def test_text(self):
-    name = 'mr.samuel.l.jackson'
-    text = 'sometexthere'
-    self.assertEqual(convert_line(name), name)
-    self.assertEqual(convert_line(text), text)
+    text = 'mr.samuel.l.jackson,sometexthere'
+    self.assertEqual(convert_line(text), ';'.join(text.split(',')))
   def test_invalid_number(self):
     numbers = '42,2.4'
     self.assertEqual(convert_line(numbers), ';'.join(numbers.split(',')))
